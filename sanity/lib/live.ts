9f09477d2e9d6a5b470bfbf67f83a94139669b98
Usama Nadeem <usamanadeemparacha@gmail.com>
2025-09-22 04:16:39 +0500

    Added API read token

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -4,6 +4,12 @@
 import { defineLive } from "next-sanity/live";
 import { client } from './client'
 
+const token = process.env.SANITY_API_TOKEN;
+if (!token) {
+  throw new Error("Missing SANITY_API_TOKEN");
+}
+
 export const { sanityFetch, SanityLive } = defineLive({
   client,
+  serverToken: token,
 });
