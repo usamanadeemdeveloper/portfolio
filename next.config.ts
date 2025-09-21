0b104d05adb2716474f9cededf1ad7a73a642cab
Usama Nadeem <usamanadeemparacha@gmail.com>
2025-09-22 02:32:23 +0500

    Removed depricated keys from next config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  optimizeCss: true,
   images: {
     remotePatterns: [
       {
